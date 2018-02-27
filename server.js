8bf8c5023c9ce6ddc578d3ddc54e68fd88d2905c
Steve Marshall <stevesonoa@gmail.com>
2018-02-26 20:59:07 -0700

    Updated for LinkedIn mk1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Require the necessary packages
 var express = require('express');
 var bodyParser = require('body-parser');
+var Linkedin = require('node-linkedin')('86iqdrrwi4ttfs', 'Mi8cFFT1VKvypzTu', 'https://linkedin-api-test.herokuapp.com/');
 
 var PORT = process.env.PORT || 3000;
 var app = express();
@@ -22,6 +23,22 @@ app.set("view engine", "handlebars");
 
 // Routes
 // =============================================================
+// Again, `res` is optional, you could pass `code` as the first parameter 
+app.get('/oauth/linkedin/callback', function(req, res) {
+    Linkedin.auth.getAccessToken(res, req.query.code, req.query.state, function(err, results) {
+        if ( err )
+            return console.error(err);
+ 
+        /**
+         * Results have something like:
+         * {"expires_in":5184000,"access_token":". . . ."}
+         */
+ 
+        console.log(results);
+        return res.redirect('/');
+    });
+});
+
 require("./controllers/burgers_controller.js")(app);
 
 app.listen(PORT, function() {
