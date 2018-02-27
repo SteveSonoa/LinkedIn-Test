610d313d1c31e56194e0d2e6b8d17871f4962b2e
Steve Marshall <stevesonoa@gmail.com>
2018-02-26 20:51:34 -0700

    Updated files to prepare for LinkedIn usage

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,5 +7,12 @@ module.exports = function(app) {
       res.render("index");
   });
 
+  // POST route for saving a new todo. We can create a todo using the data on req.body
+  app.post("/api/burgers", function(req, res) {
+    
+
+      res.end();
+    
+  });
 
 };
